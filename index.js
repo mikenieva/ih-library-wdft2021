186f7a6d0e4ae63319c7f95a8978fbbf23089be2
Mike Nieva <m@mikenieva.com>
2021-07-16 13:11:48 -0500

    Libros generados

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ const Book          = require('./models/Book')
 // 2. MIDDLEWARES
 require('dotenv').config()
 
+
+mongoose.connect(process.env.MONGODB, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+    .then(() => {console.log("Conectados a MongoDB")})
+    .catch((e) => console.log(e))
+
+
 app.use(express.static('public'))
 app.set("view engine", "hbs")
 
@@ -15,6 +25,43 @@ app.set("view engine", "hbs")
 app.get("/", (req,res) => {
     res.render("index")
 })
+
+app.get("/books", (req, res) => {
+    
+    Book.find({})
+        .then((librosEncontrados) => {
+            console.log(librosEncontrados) // [{...},{...},{...}] 
+
+            res.render("books", {
+                libros: librosEncontrados
+            })
+        })
+        .catch((e) => {
+            console.log(e)
+        })
+
+
+})
+
+app.get("/books/:bookId", (req, res) => {
+    console.log("Este es el req.params:", req.params)
+    const { bookId } = req.params
+
+    Book.findById(bookId)
+        .then(singleBook => {
+            console.log(`Libro encontrado:`, singleBook)
+
+            res.render("singleBook", {
+                libro: singleBook
+            })        
+
+        })
+        .catch(e => console.log(e))
+
+
+    
+})
+
 
 // 4. SERVIDOR
 
